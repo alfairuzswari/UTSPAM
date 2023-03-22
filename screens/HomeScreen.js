bca13bdce1d2ae5095dc9a5596c29bdd2a1eedec
M. Al Fairuz Swari <103370209+alfairuzswari@users.noreply.github.com>
2023-03-22 22:36:08 +0700

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,1 +1,183 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+} from "react-native"; 
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, decrementQuantity, incrementQuantity, removeFromCart } from "../CartReducer";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const HomeScreen = ({navigation}) => {
+  const cart = useSelector((state) => state.cart.cart);
+  console.log(cart);
+  const dispatch = useDispatch();
+  const images = [
+    {
+      id: "0",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqg_OBzcVDnKHv1d3hyVk_WlCo43pzit4CJQ&usqp=CAU",
+      name: "icecream",
+    },
+    {
+      id: "1",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT85O96gPiso_j2gaS0cePTBY4mCR3pumV6tw&usqp=CAU",
+      name: "biscuit",
+    },
+    {
+      id: "2",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSicQWeRoxxLEr1RLIp8dJtw-NQvSE4xtlhwA&usqp=CAU",
+      name: "chocolate",
+    },
+  ];
+  const addItemToCart = (item) => {
+    dispatch(addToCart(item));
+  };
+  const removeItemFromCart = (item) => {
+    dispatch(removeFromCart(item));
+  };
+  const increaseQuantity = (item) => {
+    dispatch(incrementQuantity(item));
+  }
+  const decreaseQuantity = (item) => {
+    if(item.quantity == 1){
+      dispatch(removeFromCart(item));
+    }else{
+      dispatch(decrementQuantity(item));
+    }
+  }
+  return (
+    <SafeAreaView>
+        <View style={{flexDirection: "row", alignItems: "center", padding: 50, justifyContent: "space-between", width: "110%"}}>
+          <Ionicons name="menu-outline" size={30} color="black" />
+          <Text style={styles.title}>Belanja Kuy!</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+            <FontAwesome name="user-o" size={30} color="black" />
+          </TouchableOpacity>
+        </View>
+      {images.map((item) => (
+        <Pressable
+          key={item.id}
+          style={{ flexDirection: "row", alignItems: "center" }}
+        >
+          <View style={{ margin: 10 }}>
+            <Image
+              style={{ width: 100, height: 100, borderRadius: 8 }}
+              source={{ uri: item.image }}
+            />
+          </View>
+          <View>
+            <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+            {cart.some((value) => value.id == item.id) ? (
+              <Pressable onPress={() => removeItemFromCart(item)}>
+                <Text
+                  style={{
+                    borderColor: "gray",
+                    borderWidth: 1,
+                    marginVertical: 10,
+                    padding: 5,
+                  }}
+                >
+                  REMOVE FROM CART
+                </Text>
+              </Pressable>
+            ) : (
+              <Pressable onPress={() => addItemToCart(item)}>
+                <Text
+                  style={{
+                    borderColor: "gray",
+                    borderWidth: 1,
+                    marginVertical: 10,
+                    padding: 5,
+                  }}
+                >
+                  ADD TO CART
+                </Text>
+              </Pressable>
+            )}
+          </View>
+        </Pressable>
+      ))}
+
+      {cart.map((item,index) => (
+        <View style={{padding:10}} key={index}>
+          <Text>{item.name}</Text>
+          <Image style={{ width: 100, height: 100, borderRadius: 8,marginTop:6 }}
+              source={{ uri: item.image }}/>
+          <Pressable
+            style={{
+              flexDirection: "row",
+              marginTop:20,
+              alignItems: "center",
+              backgroundColor: "#FF3366",
+              borderRadius: 5,
+              width: 120,
+            }}
+          >
+            <Pressable onPress={() => decreaseQuantity(item)}>
+              <Text
+                style={{
+                  fontSize: 25,
+                  color: "white",
+                  paddingHorizontal: 10,
+                }}
+              >
+                -
+              </Text>
+            </Pressable>
+
+            <Pressable>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: "white",
+                  paddingHorizontal: 10,
+                }}
+              >
+                {item.quantity}
+              </Text>
+            </Pressable>
+
+            <Pressable onPress={() => increaseQuantity(item)}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: "white",
+                  paddingHorizontal: 10,
+                }}
+              >
+                +
+              </Text>
+            </Pressable>
+          </Pressable>
+        </View>
+      ))}
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 50,
+    justifyContent: "space-between",
+    width: "110%",
+  },
+  title: {
+    color: "black",
+    fontSize: 35,
+  },
+});
